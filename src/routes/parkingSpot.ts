7d63a6daeb0fe97444af6b90c5a12b77f412600e
Michele Biondi <michelebiondi01@gmail.com>
2019-11-30 16:31:31 +0100

    Removed preliminary connection

--- src/routes/parkingSpot.ts
+++ src/routes/parkingSpot.ts
@@ -8,35 +8,33 @@ export const routeName = '/parkingSpot';
 // create typeorm connection
 createConnection()
     .then(async () => {
-        const parkingSpotRepository = getRepository(ParkingSpot);
-
         router.get('/', async (req, res) => {
-            return res.send(await parkingSpotRepository.find());
+            return res.send(await getRepository(ParkingSpot).find());
         });
 
         router.get('/presence', async (req, res) => {
-            const parkingSpotsAvailable = await parkingSpotRepository.find({
+            const parkingSpotsAvailable = await getRepository(ParkingSpot).find({
                 where: { status: true },
             });
             return res.send(parkingSpotsAvailable);
         });
 
         router.get('/:id', async (req, res) => {
-            const results = await parkingSpotRepository.findOne(req.params.id);
+            const results = await getRepository(ParkingSpot).findOne(req.params.id);
             if (results === undefined) return res.sendStatus(404);
             else return res.send(results);
         });
 
         router.post('/', async (req, res) => {
             //Check if the parking spot already exists
-            const alreadyExistsParkingSpot = await parkingSpotRepository.findOne({
+            const alreadyExistsParkingSpot = await getRepository(ParkingSpot).findOne({
                 latitude: req.body.latitude,
                 longitude: req.body.longitude,
             });
             if (alreadyExistsParkingSpot === undefined) {
                 //Save the parking spot
-                const parkingSpot = await parkingSpotRepository.create(req.body);
-                const results = await parkingSpotRepository.save(parkingSpot);
+                const parkingSpot = await getRepository(ParkingSpot).create(req.body);
+                const results = await getRepository(ParkingSpot).save(parkingSpot);
                 return res.send(results);
             } else {
                 //Already exists a parking spot in this position (lat & lot is already in the parking)
@@ -45,18 +43,18 @@ createConnection()
         });
 
         router.get('/:id/presence', async (req, res) => {
-            const results = await parkingSpotRepository.findOne(req.params.id);
+            const results = await getRepository(ParkingSpot).findOne(req.params.id);
             if (results === undefined) return res.sendStatus(404);
             else if (results.status) return res.send({ status: true });
             else return res.send({ status: false });
         });
 
         router.put('/:id/presence', async (req, res) => {
-            const parkingSpotToUpdate = await parkingSpotRepository.findOne(req.params.id);
+            const parkingSpotToUpdate = await getRepository(ParkingSpot).findOne(req.params.id);
             if (parkingSpotToUpdate === undefined) return res.sendStatus(404);
             else {
                 parkingSpotToUpdate.status = !parkingSpotToUpdate.status;
-                parkingSpotRepository.save(parkingSpotToUpdate);
+                getRepository(ParkingSpot).save(parkingSpotToUpdate);
                 res.send(parkingSpotToUpdate);
             }
         });

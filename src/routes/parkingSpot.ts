b94c05ed3ee16d64432dc58627bc5b3020106472
Michele Biondi <michelebiondi01@gmail.com>
2019-11-30 17:26:25 +0100

    changed parking spot route file formatting

--- src/routes/parkingSpot.ts
+++ src/routes/parkingSpot.ts
@@ -5,17 +5,11 @@ import ParkingSpotController from './../controller/ParkingSpotController';
 export const router = express.Router();
 
 const controller = new ParkingSpotController();
-
 export const routeName = controller.path;
 
 router.get('/', controller.getAllParkingSpots);
-
 router.get('/presence', controller.getAllParkingSpotsPresence);
-
 router.get('/:id', controller.getParkingSpot);
-
 router.post('/', controller.createParkingSpot);
-
 router.get('/:id/presence', controller.getParkingSpotPresence);
-
 router.put('/:id/presence', controller.putParkingSpotPresence);

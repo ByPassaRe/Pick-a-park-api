86be99473191b01f53ea26c8155d23da8461b1d1
Michele Biondi <michelebiondi01@gmail.com>
2019-12-01 23:44:41 +0100

    Add 2 simple tests for app file

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,12 +1,12 @@
-/* eslint-disable @typescript-eslint/ban-ts-ignore */
 import { connectDb, createApp, startServer } from './../src/app';
 
 describe('Main index file', () => {
-    it('should run app if database succeded to connect', async () => {
+    it('should not run app if database fails to connect', async () => {
         // @ts-ignore
         connectDb = jest.fn(() => false);
         // @ts-ignore
         createApp = jest.fn();
+
         await startServer();
         expect(createApp).toBeCalledTimes(0);
     });
@@ -16,6 +16,7 @@ describe('Main index file', () => {
         connectDb = jest.fn(() => true);
         // @ts-ignore
         createApp = jest.fn();
+
         await startServer();
         expect(createApp).toBeCalledTimes(1);
     });
